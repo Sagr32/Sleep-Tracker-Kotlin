9ca49cd4f529c3c20ce65b892b88248af2ac1feb
sagr32 <sagr3272@gmail.com>
2022-07-17 03:03:59 +0200

    Add SleepDatabase

--- app/src/main/java/com/sagr/sleeptracker/database/SleepDatabase.kt
+++ app/src/main/java/com/sagr/sleeptracker/database/SleepDatabase.kt
@@ -15,3 +15,42 @@
  */
 
 package com.sagr.sleeptracker.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+
+abstract val sleepDatabaseDao:SleepDatabaseDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context): SleepDatabase {
+
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE = buildDatabase(context)
+                }
+            }
+            return INSTANCE!!
+        }
+
+        private fun buildDatabase(context: Context): SleepDatabase {
+            return Room.databaseBuilder(
+                context.applicationContext,
+                SleepDatabase::class.java,
+                "sleep_database"
+            )
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+    }
+
+
+}
