880a61a46d88da770750825c6d26f8a676d5d578
sagr32 <sagr3272@gmail.com>
2022-07-20 02:43:52 +0200

    Add RecyclerView in SleepTracker

---
+++ app/src/main/java/com/sagr/sleeptracker/sleeptracker/SleepTrackerAdapter.kt
@@ -0,0 +1,37 @@
+package com.sagr.sleeptracker.sleeptracker
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.sagr.sleeptracker.R
+import com.sagr.sleeptracker.TextItemViewHolder
+import com.sagr.sleeptracker.database.SleepNight
+
+
+class SleepTrackerAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
+
+    var data = listOf<SleepNight>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item = data[position]
+
+        holder.textView.text = item.sleepQuality.toString()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater
+            .inflate(R.layout.text_item_view, parent, false) as TextView
+        return TextItemViewHolder(view)
+    }
+
+
+}
